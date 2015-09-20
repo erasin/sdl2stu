f648734d5afa933787ccba1202f0cd8881a69d7d
Erasin <erasinoo@gmail.com>
2015-09-20 14:06:27 +0800

    just Surface

--- main.go
+++ main.go
@@ -16,11 +16,11 @@ var windowTitle = "SDL2 Tutorial"
 func main() {
 	//
 	var window *sdl.Window
-	var renderer *sdl.Renderer
+	// var renderer *sdl.Renderer
 	var screenSurface *sdl.Surface
 	var err error
-	var src, dst sdl.Rect
-	var texture *sdl.Texture
+	// var src, dst sdl.Rect
+	// var texture *sdl.Texture
 
 	// 初始化
 	if err = sdl.Init(sdl.INIT_AUDIO); err != nil {
@@ -35,37 +35,43 @@ func main() {
 	}
 	defer window.Destroy()
 
-	// 渲染器
-	renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_PRESENTVSYNC)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "无法创建渲染器 %s\n", err)
-		os.Exit(2)
-	}
-	defer renderer.Destroy()
+	screenSurface, _ = window.GetSurface()
+	maprgb := sdl.MapRGB(screenSurface.Format, 173, 22, 196)
+	screenSurface.FillRect(&sdl.Rect{X: 0, Y: 0, W: int32(screenWidth), H: int32(screenHeight)}, maprgb)
 
-	var rmask uint32 = 0
-	var gmask uint32 = 32
-	var bmask uint32 = 32
-	var amask uint32 = 32
-	var x, y int32 = 0, 0
-
-	screenSurface, _ = sdl.CreateRGBSurface(0, int32(32), int32(screenHeight), 32, rmask, gmask, bmask, amask)
-
-	texture, err = renderer.CreateTextureFromSurface(screenSurface)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "加载失败texture: %s\n", err)
-	}
-	defer texture.Destroy()
-
-	// 全屏加载
-	src = sdl.Rect{X: 0, Y: 0, W: int32(screenWidth), H: int32(screenHeight)}
-	dst = sdl.Rect{X: x, Y: y, W: int32(screenWidth), H: int32(screenHeight)}
-
-	renderer.Clear()
-	// renderer.SetDrawColor(255, 0, 0, 0)
-	// renderer.FillRect(&sdl.Rect{X: 0, Y: 0, W: int32(screenWidth), H: int32(screenHeight)})
-	renderer.Copy(texture, &src, &dst)
-	renderer.Present()
+	window.UpdateSurface()
+	//
+	// // 渲染器
+	// renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_PRESENTVSYNC)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "无法创建渲染器 %s\n", err)
+	// 	os.Exit(2)
+	// }
+	// defer renderer.Destroy()
+	//
+	// var rmask uint32 = 0
+	// var gmask uint32 = 32
+	// var bmask uint32 = 32
+	// var amask uint32 = 32
+	// var x, y int32 = 0, 0
+	//
+	// screenSurface, _ = sdl.CreateRGBSurface(0, int32(32), int32(screenHeight), 32, rmask, gmask, bmask, amask)
+	//
+	// texture, err = renderer.CreateTextureFromSurface(screenSurface)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "加载失败texture: %s\n", err)
+	// }
+	// defer texture.Destroy()
+	//
+	// // 全屏加载
+	// src = sdl.Rect{X: 0, Y: 0, W: int32(screenWidth), H: int32(screenHeight)}
+	// dst = sdl.Rect{X: x, Y: y, W: int32(screenWidth), H: int32(screenHeight)}
+	//
+	// renderer.Clear()
+	// // renderer.SetDrawColor(255, 0, 0, 0)
+	// // renderer.FillRect(&sdl.Rect{X: 0, Y: 0, W: int32(screenWidth), H: int32(screenHeight)})
+	// renderer.Copy(texture, &src, &dst)
+	// renderer.Present()
 
 	sdl.Delay(5000)
 }
